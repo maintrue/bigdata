df52f14fc82c2a4da94376eb0c255babfdcd6afe
maintrue <75486726+maintrue@users.noreply.github.com>
2022-07-07 15:21:10 +0800

    report by sql

--- dmp/src/main/scala/com/main/dmp/report/DailyReportRunner.scala
+++ dmp/src/main/scala/com/main/dmp/report/DailyReportRunner.scala
@@ -15,7 +15,7 @@ object DailyReportRunner {
       .getOrCreate()
 
     val processors: List[ReportProcessor] = List(
-      NewRegionReportProcessor
+      AdsRegionProcessor
     )
 
     for (processor <- processors) {
