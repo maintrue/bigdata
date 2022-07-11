ef1827583e75bfc4d6c5b596789b79b09c12c31d
maintrue <75486726+maintrue@users.noreply.github.com>
2022-07-11 23:43:17 +0800

    flink broatcast

--- flink/src/main/java/com/main/bd/flink/broadcast/Broadcast.java
+++ flink/src/main/java/com/main/bd/flink/broadcast/Broadcast.java
@@ -1,4 +1,4 @@
-package com.main.bd.flink.other;
+package com.main.bd.flink.broadcast;
 
 import org.apache.flink.api.common.functions.RichMapFunction;
 import org.apache.flink.api.java.ExecutionEnvironment;
