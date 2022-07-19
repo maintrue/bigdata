5d44d4902ef3bc09a3510320d0417eb033d49c6b
maintrue <75486726+maintrue@users.noreply.github.com>
2022-07-19 15:06:13 +0800

    用户画像加文档

---
+++ personas/src/main/java/com/main/Ticket.java
@@ -0,0 +1,22 @@
+package com.main;
+
+import java.util.Map;
+
+public class Ticket {
+
+    public static void main(String[] args) {
+
+        //1.先按ABCD四个区域划分票数
+
+
+
+    }
+
+    private Map<Integer,String> ticketByPartition(){
+
+        
+
+        return null;
+    }
+
+}
--- scala/src/main/scala/com/main/bd/scala/Test.scala
+++ scala/src/main/scala/com/main/bd/scala/Test.scala
@@ -4,7 +4,7 @@ object Test {
 
   def main(args: Array[String]): Unit = {
 
-    println(1)
+
 
   }
 
